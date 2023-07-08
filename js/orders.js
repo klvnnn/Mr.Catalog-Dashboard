777b9d94540fded6a8db185a8a01b52581954eea
klvnnn <klvnnnyatiga@gmail.com>
2023-07-08 14:18:14 +0700

    [UPLOAD] Mr.Catalog Admin Dashboard

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName : 'Supra GTR 125',
+        productBrand : 'Honda',
+        paymentStatus : 'Due',
+        status : 'Pending'
+    },
+    {
+        productName : 'Vixion Cafe Raceer',
+        productBrand : 'Yamaha',
+        paymentStatus : 'Refunded',
+        status : 'Rejected'
+    },
+    {
+        productName : 'Ducati 150',
+        productBrand : 'Ducati',
+        paymentStatus : 'Paid',
+        status : 'Success'
+    },
+]
